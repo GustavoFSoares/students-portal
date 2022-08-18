59e0429485f2c75e9dc97e8b3213c05bd7975bf7
Gustavo Soares <gustavo.soares.freelancer@gmail.com>
2022-08-17 22:42:36 -0300

    ✨ feat: created card component

---
+++ src/components/atoms/AvCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="av-card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "av-card",
+};
+</script>
+
+<style lang="scss" scoped>
+.av-card {
+  background: #fff;
+  border-radius: 8px;
+  padding: 15px 20px;
+}
+</style>
