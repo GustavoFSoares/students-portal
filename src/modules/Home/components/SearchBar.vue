c6a7152df2a07d75ed0a92bea5e029b6273a4478
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-08-30 22:44:07 -0300

    ✨ feat: created search bar events interactions

---
+++ src/modules/Home/components/SearchBar.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="search-bar" :class="{ 'search-bar--active': isActiveShowBar }">
+    <div class="search-bar__input-wrapper">
+      <transition name="search-bar__input">
+        <QInput
+          v-show="isActiveShowBar"
+          class="search-bar__input"
+          dense
+          filled
+          ref="inputElement"
+          :model-value="modelValue"
+          @update:modelValue="handleInput"
+        />
+      </transition>
+    </div>
+
+    <div class="search-bar__button-wrapper">
+      <QBtn
+        class="search-bar__button"
+        padding="sm"
+        icon="search"
+        color="white"
+        :disable="!!modelValue"
+        @click="toggleSearchBar"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  name: "search-bar",
+  props: {
+    modelValue: {
+      type: String,
+      default: null,
+    },
+  },
+  emits: ["update:modelValue"],
+  setup(props, ctx) {
+    const searchInput = ref(null);
+    const showActiveBar = ref(!!props.modelValue);
+    const inputElement = ref(null);
+
+    const isActiveShowBar = computed(() => {
+      return showActiveBar.value || !!props.modelValue;
+    });
+
+    const handleInput = (value) => {
+      ctx.emit("update:modelValue", value);
+    };
+
+    const toggleSearchBar = async () => {
+      showActiveBar.value = !showActiveBar.value;
+
+      if (showActiveBar.value) {
+        await inputElement.value;
+        inputElement.value.focus();
+      }
+    };
+
+    return {
+      searchInput,
+      showActiveBar,
+      inputElement,
+      isActiveShowBar,
+      handleInput,
+      toggleSearchBar,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.search-bar {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+
+  &__input {
+    &-enter-active {
+      animation: bounce-in 0.8s;
+    }
+    &-leave-active {
+      animation: bounce-in 0.8s reverse;
+    }
+
+    @keyframes bounce-in {
+      0% {
+        width: 0px;
+        opacity: 0;
+      }
+      1% {
+        width: 1%;
+        opacity: 0;
+      }
+      10% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 1;
+        width: 100%;
+      }
+    }
+
+    &-wrapper {
+      flex-grow: 1;
+    }
+  }
+
+  &__button {
+    color: $secondary !important;
+  }
+
+  &--active & {
+    &__input {
+      visibility: initial;
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/modules/Home/pages/CoursesPage.vue
+++ src/modules/Home/pages/CoursesPage.vue
@@ -1,9 +1,35 @@
 <template>
-  <article class="courses-page">courses-page</article>
+  <article class="courses-page">
+    <SearchBar v-model="searchText" />
+    {{ searchText }}
+
+    <div>courses-page</div>
+  </article>
 </template>
 
 <script>
+import { ref } from "vue";
+import SearchBar from "../components/SearchBar.vue";
+
 export default {
   name: "courses-page",
+  components: {
+    SearchBar,
+  },
+  setup() {
+    const searchText = ref("321");
+
+    return {
+      searchText,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.courses-page {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+</style>
