7e7c556245d35e26b8f384353439f535a142c31a
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-09-14 10:11:22 -0300

    🚧 feat: colorizing cards

--- src/modules/Home/components/InsightsPage/RankingCard.vue
+++ src/modules/Home/components/InsightsPage/RankingCard.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="ranking-card">
+  <div :class="['ranking-card', `ranking-card--rank-${userRank}`]">
     <div class="ranking-card__wrapper">
       <div class="ranking-card__content">
         <div class="ranking-card__rank">
-          <h6 class="ranking-card__rank-number">
-            {{ userRank }}
-          </h6>
+          <div class="rank-number">
+            <h6 class="rank-number__text">{{ userRank }}º</h6>
+          </div>
 
-          <QIcon name="img:/svg/medal.svg" class="ranking-card__rank-medal" />
+          <QIcon
+            v-if="false && isWinner && !isFirst"
+            class="rank-medal"
+            name="img:/svg/medal.svg"
+          />
         </div>
 
         <div class="ranking-card__user-image">
           <QIcon
-            class="ranking-card__user-image-crown"
-            name="img:assets/icons/crown.svg"
             v-if="false && isFirst"
+            class="ranking-card__user-image-crown"
+            name="img:/svg/crown.svg"
           />
 
           <img
@@ -23,7 +27,7 @@
           />
         </div>
 
-        <div class="ranking-card__points">{{ userPoints }}</div>
+        <div class="ranking-card__points">{{ userPoints }}pts</div>
       </div>
 
       <div class="ranking-card__user-name">
@@ -82,17 +86,62 @@ export default {
     border: 2px solid $secondary;
     border-radius: 8px;
 
+    display: flex;
+    flex-direction: column;
+
     height: 65%;
   }
 
   &__content {
     display: flex;
     justify-content: space-evenly;
+    align-items: center;
+    flex-grow: 1;
+  }
+
+  &__rank {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    gap: 10px;
+
+    .rank-number {
+      min-width: 25px;
+      min-height: 25px;
+
+      border-radius: 50%;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      background: $secondary;
+      box-shadow: 0 3px #7c7c7c;
+
+      &__text {
+        color: $white;
+        font-size: 15px;
+        font-weight: $font-weight-bold;
+
+        padding: 2px;
+      }
+    }
+
+    .rank-medal {
+      font-size: 20px;
+    }
+
+    &:has(.rank-medal) {
+      margin-top: 25px;
+    }
   }
 
   &__user-image {
     display: flex;
     flex-direction: column;
+    // padding: 20px 0;
 
     img {
       width: 50px;
@@ -112,6 +161,7 @@ export default {
     &-text {
       padding: 0 8px;
       font-weight: $font-weight-bold;
+      font-size: 15px;
     }
 
     &:before,
@@ -121,6 +171,50 @@ export default {
       width: 100%;
       border-radius: 8px;
       background: $secondary;
+    }
+  }
+
+  $rank-colors: (
+    1: (
+      default: $yellow-8,
+      boxShadow: $yellow-9,
+      height: 100%,
+    ),
+    2: (
+      default: $grey-13,
+      boxShadow: $grey-14,
+      height: 80%,
+    ),
+    3: (
+      default: $deep-orange-4,
+      boxShadow: $deep-orange-6,
+      height: 70%,
+    ),
+  );
+
+  @each $rank, $rank-data in $rank-colors {
+    &--rank-#{$rank} & {
+      &__wrapper {
+        height: map-get($rank-data, "height");
+
+        border-color: map-get($rank-data, "default");
+      }
+
+      &__rank .rank-number {
+        background: map-get($rank-data, "default");
+        box-shadow: 0 3px map-get($rank-data, "boxShadow");
+      }
+
+      &__user-image img {
+        border-color: map-get($rank-data, "default");
+      }
+
+      &__user-name {
+        &:before,
+        &:after {
+          background: map-get($rank-data, "default");
+        }
+      }
     }
   }
 }
