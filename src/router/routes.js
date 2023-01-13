1602e1cc3c1e17abd8d21cc084f70049233f38c0
Gustavo Soares <gustavo.fe.soares@gmail.com>
2023-01-13 14:46:09 -0300

    fix: root page redirect

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,7 @@ const routes = [
   {
     path: "",
     name: "",
+    redirect: { name: "home" },
     component: () => import("layouts/CleanLayout.vue"),
     children: [...HomeRoutes, ...UserRoutes, ...ActivitiesRoutes],
   },
