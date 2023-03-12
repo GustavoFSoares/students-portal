0c81ec42297e0a14b5d4f8f0b5e0a1c2c77e125b
Gustavo Soares <gustavo.fe.soares@gmail.com>
2023-03-11 21:59:24 -0300

    feat: created certificate page

--- src/store/modules/certificates/actions.js
+++ src/store/modules/certificates/actions.js
@@ -10,16 +10,18 @@ export default {
         data: { data },
       } = await api.post("alunos/buscar-certificados");
 
-      const preparedSertificates = data.map((certificate) => ({
-        id: certificate.certificado_trilha.certificado.id,
-        name: certificate.certificado_trilha.certificado.titulo,
-        description: certificate.certificado_trilha.certificado.conteudo,
-        trailId: certificate.certificado_trilha.trilha_id,
-        path: certificate.certificado_trilha.trilha.capa
-          ? certificate.certificado_trilha.trilha.capa.path
-          : undefined,
-        completedDate: new Date(certificate.certificado_trilha.updated_at),
-      }));
+      const preparedSertificates = data.map((certificate) => {
+        return {
+          id: certificate.certificado_trilha.certificado.id,
+          name: certificate.certificado_trilha.certificado.titulo,
+          description: certificate.certificado_trilha.certificado.conteudo,
+          activity: {
+            id: certificate.certificado_trilha.trilha.id,
+            name: certificate.certificado_trilha.trilha.nome,
+          },
+          conclusionDate: new Date(certificate.certificado_trilha.updated_at),
+        };
+      });
 
       commit("SET_CERTIFICATES", preparedSertificates);
 
