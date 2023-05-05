71f4037178cf9f72f1c181cb44bbee77f613397d
Gustavo Soares <gustavo.fe.soares@gmail.com>
2023-05-05 18:10:02 -0300

    chore: changed component name

--- src/modules/Insights/pages/AchievementsPage.vue
+++ src/modules/Insights/pages/AchievementsPage.vue
@@ -1,84 +1,50 @@
 <template>
-  <article class="insights-achievements-page">
+  <article class="insights-achievements">
     <AvPageSection :title="$t(`${I18N_PATH}.title`)" @close="handleGoBackPage">
-      <template #default>
-        <CertificatesPageHeader
-          class="insights-achievements-page__header"
-          v-model:current-tab="currentTab"
-          v-model:order-selected="orderSelected"
+      <div class="insights-achievements__list">
+        <ConquestCard
+          v-for="(conquestData, conquestKey) in conquests"
+          :key="conquestKey"
+          :title="conquestData.name"
+          :description="conquestData.description"
+          :badge="conquestData.path"
+          :obtained="conquestData.obtained"
         />
-
-        <div class="insights-achievements-page__content">
-          <div
-            v-if="hasCertificates"
-            class="insights-achievements-page__certificates-list"
-          >
-            <CertificatesPageCard
-              v-for="(certificate, certificateIndex) in certificates"
-              :key="`certificate-${certificateIndex}`"
-              :title="certificate.name"
-              :description="certificate.description"
-              :image="certificate.path"
-              :completed-date="certificate.completedDate"
-            />
-          </div>
-
-          <div v-else class="insights-achievements-page__no-certificates">
-            <h5 class="insights-achievements-page__no-certificates-text">
-              {{ $t(`${I18N_PATH}.noCertificates`) }}
-            </h5>
-          </div>
-        </div>
-      </template>
+      </div>
     </AvPageSection>
   </article>
 </template>
 
 <script>
+import { computed } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { computed, onMounted, ref } from "vue";
 
 import AvPageSection from "molecules/AvPageSection.vue";
+import ConquestCard from "../components/AchievementCard.vue";
 
-import CertificatesPageCard from "../components/CertificatesPageCard.vue";
-import CertificatesPageHeader from "../partials/CertificatesPageHeader.vue";
-
-const I18N_PATH = "modules.home.insightsPage.pages.certificatesPage";
-
+const I18N_PATH = "modules.home.insightsPage.pages.conquestPage";
 export default {
-  name: "InsigtCertificatePage",
+  name: "InsightConquestPage",
   components: {
     AvPageSection,
-    CertificatesPageCard,
-    CertificatesPageHeader,
+    ConquestCard,
   },
   setup() {
+    const $router = useRouter();
     const $store = useStore();
-    const $route = useRouter();
 
-    const currentTab = ref(undefined);
-    const orderSelected = ref(undefined);
-
-    const certificates = computed(
-      () => $store.getters["CertificatesModule/getCertificates"]
+    const conquests = computed(
+      () => $store.getters["AchievementsModule/achievements"]
     );
-    const hasCertificates = computed(() => certificates.value.length > 0);
 
     const handleGoBackPage = () => {
-      $route.push({ name: "home.insights" });
+      $router.push({ name: "home.insights" });
     };
-
-    onMounted(async () => {
-      await $store.dispatch("CertificatesModule/loadCertificates");
-    });
 
     return {
       I18N_PATH,
-      currentTab,
-      certificates,
-      orderSelected,
-      hasCertificates,
+      conquests,
       handleGoBackPage,
     };
   },
@@ -86,23 +52,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.insights-achievements-page {
-  &__content {
-    margin-bottom: 60px;
-  }
-
-  &__no-certificates {
-    display: flex;
-    justify-content: center;
-
-    &-text {
-      font-size: 24px;
-      font-weight: $font-weight-semibold;
-      text-align: center;
-    }
-  }
-
-  &__certificates-list {
+.insights-achievements {
+  &__list {
     display: grid;
 
     gap: 15px;
---
+++ src/modules/Insights/pages/CertificatesPage.vue
@@ -0,0 +1,117 @@
+<template>
+  <article class="insights-certificates-page">
+    <AvPageSection :title="$t(`${I18N_PATH}.title`)" @close="handleGoBackPage">
+      <template #default>
+        <CertificatesPageHeader
+          class="insights-certificates-page__header"
+          v-model:current-tab="currentTab"
+          v-model:order-selected="orderSelected"
+        />
+
+        <div class="insights-certificates-page__content">
+          <div
+            v-if="hasCertificates"
+            class="insights-certificates-page__certificates-list"
+          >
+            <CertificatesPageCard
+              v-for="(certificate, certificateIndex) in certificates"
+              :key="`certificate-${certificateIndex}`"
+              :title="certificate.name"
+              :description="certificate.description"
+              :image="certificate.path"
+              :completed-date="certificate.completedDate"
+            />
+          </div>
+
+          <div v-else class="insights-certificates-page__no-certificates">
+            <h5 class="insights-certificates-page__no-certificates-text">
+              {{ $t(`${I18N_PATH}.noCertificates`) }}
+            </h5>
+          </div>
+        </div>
+      </template>
+    </AvPageSection>
+  </article>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { computed, onMounted, ref } from "vue";
+
+import AvPageSection from "molecules/AvPageSection.vue";
+
+import CertificatesPageCard from "../components/CertificatesPageCard.vue";
+import CertificatesPageHeader from "../partials/CertificatesPageHeader.vue";
+
+const I18N_PATH = "modules.home.insightsPage.pages.certificatesPage";
+
+export default {
+  name: "InsigtCertificatePage",
+  components: {
+    AvPageSection,
+    CertificatesPageCard,
+    CertificatesPageHeader,
+  },
+  setup() {
+    const $store = useStore();
+    const $route = useRouter();
+
+    const currentTab = ref(undefined);
+    const orderSelected = ref(undefined);
+
+    const certificates = computed(
+      () => $store.getters["CertificatesModule/getCertificates"]
+    );
+    const hasCertificates = computed(() => certificates.value.length > 0);
+
+    const handleGoBackPage = () => {
+      $route.push({ name: "home.insights" });
+    };
+
+    onMounted(async () => {
+      await $store.dispatch("CertificatesModule/loadCertificates");
+    });
+
+    return {
+      I18N_PATH,
+      currentTab,
+      certificates,
+      orderSelected,
+      hasCertificates,
+      handleGoBackPage,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.insights-certificates-page {
+  &__content {
+    margin-bottom: 60px;
+  }
+
+  &__no-certificates {
+    display: flex;
+    justify-content: center;
+
+    &-text {
+      font-size: 24px;
+      font-weight: $font-weight-semibold;
+      text-align: center;
+    }
+  }
+
+  &__certificates-list {
+    display: grid;
+
+    gap: 15px;
+    grid-template-columns: 1fr;
+
+    @media (min-width: $breakpoint-tablet) {
+      gap: 30px 15px;
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+}
+</style>
