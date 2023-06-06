1655e2b6a70fe0cbbae5cefcc4d0ae3cfc14a4c8
Gustavo Soares <gustavo.fe.soares@gmail.com>
2023-06-06 11:24:02 -0300

    feat: created timer component

---
+++ src/components/molecules/AvTimer.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="av-timer">
+    <QCircularProgress
+      class="av-timer__progress text-white"
+      show-value
+      font-size="16px"
+      :value="currentPercentage"
+      size="60px"
+      :thickness="0.15"
+      color="white"
+      track-color="transparent"
+    >
+      <div class="av-timer__value">
+        {{ currentTime }}
+      </div>
+    </QCircularProgress>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted, ref, toRef } from "vue";
+
+const $emits = defineEmits(["end-time"]);
+const props = defineProps({
+  startTime: {
+    type: Number,
+    required: true,
+  },
+  autoStart: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+let interval = null;
+const currentTime = ref(props.startTime);
+
+const currentPercentage = computed(
+  () => (currentTime.value / props.startTime) * 100
+);
+
+const handleStart = () => {
+  interval = setInterval(() => {
+    currentTime.value -= 1;
+
+    if (currentTime.value <= 0) {
+      handleEndTimer();
+    }
+    // console.log("HERE", new Date().toLocaleTimeString());
+  }, 1000);
+};
+
+const handleEndTimer = () => {
+  clearInterval(interval);
+  $emits("end-time");
+};
+
+onMounted(() => {
+  if (props.autoStart) {
+    handleStart();
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.av-timer {
+  background: hsla(201, 10%, 29%, 0.5);
+  border-radius: 1000px;
+  overflow: hidden;
+  padding: 2px;
+
+  &__progress {
+  }
+
+  &__value {
+    font-weight: $font-weight-bold;
+  }
+}
+</style>
